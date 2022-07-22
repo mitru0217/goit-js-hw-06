88c5736680587191d7c9c6eed4aa82c4ff7e6a4b
mitru0217 <mitru0217@gmail.com>
2022-07-22 23:54:55 +0300

    Update task-02.js
    task-2 done.

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const itemContainer = document.querySelector("#ingredients");
+
+const elements = ingredients.map(ingredient => {
+const itemEl = document.createElement("li");
+itemEl.classList.add('item');
+  itemEl.textContent = ingredient;
+  
+  return itemEl;
+})
+
+itemContainer.append(...elements);
+
+console.log(itemContainer);
