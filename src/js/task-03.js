fe59fde5beec0a599c24ae27688e0f6a8633a510
mitru0217 <mitru0217@gmail.com>
2022-07-23 09:11:28 +0300

    Update task-03.js
    task-3 in progress

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,40 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const imagesContainer = document.querySelector(".gallery");
+
+// const creatListOfImages = images.map(function (image) {
+//   const newItem = document.createElement("li");
+//   const newImage = document.createElement('img');
+//   newItem.append(newImage);
+//   newImage.alt = image.alt;
+//   newImage.src = image.url;
+
+//   return newItem;
+// })
+//   .join(""); 
+// console.log(creatListOfImages);
+  
+// imagesContainer.insertAdjacentHTML("afterbegin", creatListOfImages); 
+
+// console.log(imagesContainer);
+
+const newimages = images.map((image) => 
+  (`<li class = images__item> ${newimages.alt = image.alt}</li>)`)
+
+)
+imagesContainer.insertAdjacentHTML("afterbegin", newimages);  
+
+// const newimages = images.map((image) => 
+//   `<li class = images__item> ${ images.map(function (image) {
+//   const newImage = document.createElement('img');
+//   newImage.alt = image.alt;
+//   newImage.src = image.url;
+
+//   return newImage;
+// })} </li>).join("");`
+
+// )
+// imagesContainer.insertAdjacentHTML("afterbegin", newimages);  
+
